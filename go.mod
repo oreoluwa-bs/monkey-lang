df9b662194771aa6b8f62dcb71c4793074dc7ee1
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-05-16 17:02:11 +0100

    feat: a repl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oreoluwa-bs/monkey
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/oreoluwa-bs/monkey/repl"
+)
+
+func main() {
+	usr, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		usr.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
